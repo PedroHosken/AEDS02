09d30fbfe60e8c40d76c7dca698a3f8306cf93cb
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2024-08-29 18:18:14 -0300

    Exercicio bugado 15

--- Verde/TP01/Q15/IsRec.java
+++ Verde/TP01/Q15/IsRec.java
@@ -19,86 +19,124 @@ public class IsRec {
         }
     }
 
-    // metodo de verificar vogais
+    // metodo de chamada de recursão
     static boolean vogais(String palavra) {
         // definir dados
-        boolean is = false;
+        int tam = palavra.length();
+        // retornar valor
+        return vogais(palavra, 0, tam);
+    }
+
+    // metodo de verificar vogais recursivamente
+    static boolean vogais(String palavra, int x, int tam) {
+        // definir dados
+        boolean is = true;// MUITO IMPORTANTE PARA RECURSIVIDADE
         char c = 'a';
-        // metodo iterativo
-        for (int x = 0; x < palavra.length(); x++) {
+        // opção de parada
+        if (x < tam) {
+
             c = palavra.charAt(x);
             if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u') || (c == 'A') || (c == 'E')
                     || (c == 'I') || (c == 'O') || (c == 'U')) {
                 is = true;
+                is = vogais(palavra, x + 1, tam);
             } else {
                 is = false;
-                x = palavra.length();
+                x = tam + 1;
             }
+
         }
         // retornar valor
         return is;
     }
 
-    // metodo de verificar vogais
+    // metodo de chamada de recursão
     static boolean consoantes(String palavra) {
         // definir dados
-        boolean is = false;
+        int tam = palavra.length() - 1;
+        // retornar valor
+        return consoantes(palavra, 0, tam);
+    }
+
+    // metodo de verificar consoantes recursivamente
+    static boolean consoantes(String palavra, int x, int tam) {
+        // definir dados
+        boolean is = true;// MUITO IMPORTANTE PARA RECURSIVIDADE
         char c = 'a';
-        // metodo iterativo
-        for (int x = 0; x < palavra.length(); x++) {
+        // ponto de parada
+        if (x <= tam) {
             c = palavra.charAt(x);
             if ((c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u') || (c == 'A') || (c == 'E')
                     || (c == 'I') || (c == 'O') || (c == 'U')) {
                 is = false;
-                x = palavra.length();
+                x = tam + 1;
             } else if (c >= '0' && c <= '9') {
                 is = false;
-                x = palavra.length();
+                x = tam + 1;
             } else if (c == '.' || c == ',') {
                 is = false;
-                x = palavra.length();
+                x = tam + 1;
             } else {
                 is = true;
+                is = consoantes(palavra, x + 1, tam);
             }
         }
         // retornar valor
         return is;
     }
 
-    // metodo de analisar inteiros
+    // metodo de chamada de recursão
     static boolean inteiros(String palavra) {
         // definir dados
-        boolean is = false;
+        int tam = palavra.length();
+        // retornar valor
+        return inteiros(palavra, 0, tam);
+    }
+
+    // metodo de analisar inteiros recursivamente
+    static boolean inteiros(String palavra, int x, int tam) {
+        // definir dados
+        boolean is = true;// MUITO IMPORTANTE PARA RECURSIVIDADE
         char c = 'a';
-        // metodo iterativo de analise de inteiros
-        for (int x = 0; x < palavra.length(); x++) {
+        // ponto de parada
+        if (x < tam) {
             c = palavra.charAt(x);
             if (c >= '0' && c <= '9') {
                 is = true;
+                is = inteiros(palavra, x + 1, tam);
             } else {
                 is = false;
-                x = palavra.length();
+                x = tam + 1;
             }
         }
         // retornar
         return is;
     }
 
-    // metodo de analisar reais
+    // metodo de chamada de recursão
     static boolean reais(String palavra) {
         // definir dados
-        boolean is = false;
-        char c = 'a';
+        int tam = palavra.length();
         int virg = 0;
-        // metodo iterativo de analise de inteiros
-        for (int x = 0; x < palavra.length(); x++) {
+        // retornar valor
+        return reais(palavra, 0, tam, virg);
+    }
+
+    // metodo de analisar reais recursivamente
+    static boolean reais(String palavra, int x, int tam, int virg) {
+        // definir dados
+        boolean is = true; // MUITO IMPORTANTE PARA RECURSIVIDADE
+        char c = 'a';
+        // ponto de parada
+        if (x < tam) {
 
             c = palavra.charAt(x);
             if (c >= '0' && c <= '9') {
                 is = true;
+                is = reais(palavra, x + 1, tam, virg);
             } else if (c == '.' || c == ',') {
                 is = true;
-                virg++;
+                is = reais(palavra, x + 1, tam, virg + 1);
             } else {
                 is = false;
                 x = palavra.length();
