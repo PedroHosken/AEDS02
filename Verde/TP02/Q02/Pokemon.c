af74afaf98fd31af69d5fc9e1dcd4b8b608f477d
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2024-10-12 20:49:06 -0300

    Exercicio em C dando erro

--- Verde/TP02/Q02/Pokemon.c
+++ Verde/TP02/Q02/Pokemon.c
@@ -17,6 +17,7 @@
 //---------Variáveis Globais----------//
 #define MAX_ABILITIES 10 // Máximo de habilidades que um Pokémon pode ter
 #define MAX_STRING 50    // Tamanho máximo das strings
+#define MAX_LINHA 100    // Tamanho máximo das strings
 
 /**
  * Struct Pokemon
@@ -227,21 +228,165 @@ void imprimir(Pokemon *p)
 }
 
 /**
+ * Metódo de ler do arquivo CSV
+ * Objetivo: ler atributos do arquivo csv e montar pokemon
+ * Como: ler cada linha do arquivo, separar alinha por vírgulas, guardar os atributos e chamar construtor passando parametros,
+ * tomando cuidado com abilities e data
+ *
+ * @param int id - montar pokemon daquele Id
+ * @param char* file - caminho do arquivo
+ * @return Pokemon* novo - retorna novo pokemon
+ *
+ * @var FILE file -  arquivo para lermos o CSV
+ * @var Pokemon* p - novo pokemon a ser construído
+ * @var char[] linha - para ler cada linha do arquivo CSV
+ * @var bool found - verifica se o id pesquisado já foi encontrado
+ */
+
+Pokemon *ler(char *filename, char* id)
+{
+
+    // definir dados
+    Pokemon *p = NULL;
+    FILE *file = fopen(filename, "rt");
+    char linha[MAX_LINHA];
+    bool found = false;
+    int Id = atoi(id);
+
+    // teste de arquivo
+    if (file == NULL)
+    {
+        printf("Erro ao abrir arquivo\n");
+        return NULL;
+    }
+
+    // ler cabeçalho
+    fgets(linha, sizeof(linha), file);
+
+    // Lê as demais linhas do arquivo
+    while (fgets(linha, sizeof(linha), file) != NULL && !found)
+    {
+        char *token = strtok(linha, ","); // separa a linha por vírgulas
+        char *atributos[12];              // array de ponteiros para atributos
+        int index = 0;
+
+        // alocar espaço para cada atributo
+        while (token != NULL)
+        {
+            atributos[index++] = strdup(token);
+            token = strtok(NULL, ",");
+        }
+
+        // Verificar id
+        if (atoi(atributos[0]) == Id)
+        {
+            found = true; // atualizar para parar repetição
+
+            // construir pokemon com os atributos
+            int Id = atoi(atributos[0]);
+            int Generation = atoi(atributos[1]);
+            char Name[MAX_STRING];
+            char Description[MAX_STRING];
+            char Type1[MAX_STRING];
+            strcpy(Name, atributos[2]);
+            strcpy(Description, atributos[3]);
+            strcpy(Type1, atributos[4]);
+
+            // Verificar se Type2 está vazio
+            char Type2[MAX_STRING];
+            if (strlen(atributos[5]) == 0)
+            {
+                strcpy(Type2, "");
+            }
+            else
+            {
+                strcpy(Type2, atributos[5]);
+            }
+
+            // Array temporário para armazenar habilidades formatadas
+            char temp[MAX_STRING] = {0};
+            int pos = 0;
+            int len = strlen(atributos[6]);
+
+            // Pré-processamento: remover colchetes e aspas simples
+            for (int x = 0; x < len; x++)
+            {
+                if (atributos[6][x] == '[' || atributos[6][x] == ']')
+                {
+                    continue; // Ignorar colchetes
+                }
+                else if (atributos[6][x] == '\'')
+                {
+                    continue; // Ignorar aspas simples
+                }
+                else
+                {
+                    temp[pos++] = atributos[6][x]; // Copiar caractere para temp
+                }
+            }
+            temp[pos] = '\0'; // Finalizar string
+
+            // Separar habilidades usando ',' como delimitador
+            char *tempAbilities[MAX_ABILITIES];
+            int abilityIndex = 0;
+            char *abilityToken = strtok(temp, ", ");
+            while (abilityToken != NULL && abilityIndex < MAX_ABILITIES)
+            {
+                tempAbilities[abilityIndex++] = strdup(abilityToken); // Alocar e copiar habilidade
+                abilityToken = strtok(NULL, ", ");
+            }
+
+            double Weight = atof(atributos[7]);
+            double Height = atof(atributos[8]);
+            int Capturerate = atoi(atributos[9]);
+            bool Islegendary = strcmp(atributos[10], "true") == 0 ? true : false;
+            char CaptureDate[MAX_STRING];
+            strcpy(CaptureDate, atributos[11]);
+
+            // Chamada para o construtor
+            p = c2(Id, Generation, Name, Description, Type1, Type2, tempAbilities, abilityIndex, Weight, Height, Capturerate, Islegendary, CaptureDate);
+
+            // Liberar memória alocada para tempAbilities
+            for (int i = 0; i < abilityIndex; i++)
+            {
+                free(tempAbilities[i]);
+            }
+
+            // Liberar os atributos alocados com strdup
+            for (int i = 0; i < 12; i++)
+            {
+                free(atributos[i]);
+            }
+        }
+    }
+
+    fclose(file); // Fechar o arquivo após terminar a leitura
+    return p;
+}
+
+/**
  * Função main
- * @version 1 testar metódos de construtor, imprimir e de free
+ * @version 1 testar metódos de construtor, imprimir e de free - CERTO
+ * @version 2 ler do arquivo CSV e montar pokemon
  *
  */
 int main()
 {
     // definir dados
     setlocale(LC_ALL, "en_US.UTF-8"); // Forçar o uso de UTF-8
-    char *abilities[] = {"Static", "Plus"};
-    // Criar um Pokémon usando o construtor com parâmetros
-    Pokemon *p = c2(181, 2, "Ampharos", "Light Pokémon", "electric", "",
-                    abilities, 2, 61.5, 1.4, 45, false, "25/05/1999");
-
-    // Imprimir os atributos do Pokémon
-    imprimir(p);
+    char *filename = "C:/Users/User11/Documents/AEDS02/Verde/TP02/Q02/pokemon.csv";
+    char id[100];      // id que vai ser lido
+    Pokemon *p = NULL; // Pokemon que vai ser construído
+    // ler id enquanto diferente de FIM
+    scanf("%s", id);
+    getchar();
+    while (strcmp(id, "FIM") != 0)
+    {
+        p = ler(filename, id);
+        imprimir(p); // imprimir cada Pokemon
+        scanf("%s", id);
+        getchar();
+    } // end while
 
     // Liberar a memória alocada
     freePokemon(p);
@@ -253,9 +398,9 @@ int main()
 /**
  * TESTES
  *
- * @version 1 - Teste de construtores e imprimir - UTF ERRADO
+ * @version 1 - Teste de construtores e imprimir - UTF CERTO
  * // definir dados
-    setlocale(LC_ALL, "UTF-8"); // setCharset
+    setlocale(LC_ALL, "en_US.UTF-8"); // Forçar o uso de UTF-8
     char *abilities[] = {"Static", "Plus"};
     // Criar um Pokémon usando o construtor com parâmetros
     Pokemon *p = c2(181, 2, "Ampharos", "Light Pokémon", "electric", "",
