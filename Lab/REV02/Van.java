4d7cd06bcd01d2f085820bd5dc338069e6d43414
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2024-11-04 15:41:53 -0300

    version 4 - OK

--- Lab/REV02/Van.java
+++ Lab/REV02/Van.java
@@ -43,6 +43,23 @@ class Van {
     }
 
     /**
+     * Metódo de Imprimir lista
+     * Objetivo: imprimir a lista percorrendo cada componente presente
+     * Como: iterativamente = for
+     * 
+     * @param ArrayLista<Van> lista
+     * 
+     * @var Van aluno - controle de cada aluno no metódo
+     * 
+     */
+    static void imprimir(ArrayList<Van> lista) {
+        // realizar metódo iterativo
+        for (Van aluno : lista) {
+            System.out.println(aluno.name + " " + aluno.reg + " " + aluno.dist);
+        }
+    }
+
+    /**
      * Função Main
      * Objetivo: Leitura do número de alunos, ler os atributos de cada aluno,
      * guardar cada um em um array, chamar metódo de ordenação, e printar lista
@@ -64,13 +81,18 @@ class Van {
      * 
      * 
      * @version 1 - testar construtores - OK
-     * @version 2 - testar ler linha e guardar atributos por espaço em branco
+     * @version 2 - testar ler linha e guardar atributos por espaço em branco - OK
+     * @version 3 - testar guardar cada atributo em uma váriavel e formar o aluno -
+     *          OK
+     * @version 4 - testar se está guradando na lista + metódo de imprimir - OK
+     * @version 5 - realizar metódo de ordenação por menor distancia da região, e
+     *          caso empate, por nome
+     * 
      */
     public static void main(String args[]) {
         // definir dados
         Scanner sc = new Scanner(System.in);
         int n = 0;
-        Van aluno = new Van();
         ArrayList<Van> lista = new ArrayList<>();
         String linha = "";
         String name = "";
@@ -79,12 +101,19 @@ class Van {
 
         // leitura do numero de alunos
         n = sc.nextInt();
+        sc.nextLine(); // Consumir a quebra de linha após a leitura do número
+
         // ler iterativamente
         for (int i = 0; i < n; i++) {
             linha = sc.nextLine();
             String[] dados = linha.split(" ");// separa cada atributo por espaço em branco e atribui a uma posição
-            System.out.println(dados[0] + dados[1] + dados[2]);
+            name = dados[0]; // guardar nome
+            R = dados[1].charAt(0);// caractere da região
+            dist = Integer.parseInt(dados[2]); // pega distancia
+            Van aluno = new Van(name, dist, R); // chama construtor
+            lista.add(aluno);
         }
+        imprimir(lista); // metódo de imprimir lista
 
         // fechar sc
         sc.close();
@@ -95,6 +124,10 @@ class Van {
      * TESTES
      * 
      * @version 1
+     * @version 2
+     *          System.out.println(dados[0] + dados[1] + dados[2]);
+     * @version 3
+     *          System.out.println(aluno.name + aluno.dist + aluno.reg);
      * 
      */
 }
