7955d2c8ef761e75874e2235934f799b5334dd3a
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2024-11-04 18:50:19 -0300

    version 2- ok

--- Lab/REV02/Dna.c
+++ Lab/REV02/Dna.c
@@ -11,6 +11,37 @@
 #include <stdlib.h>
 
 /**
+ * Metódo compare
+ * Objetivo: comparar o array ideal com a sequencia, e retornar numero de mov min
+ * Como: passar ambos arrays como referencia, comparar cada posição do array, de forma iterativa,
+ * caso diferente atualizar variavel dif, retornar por fim dif/2
+ *
+ * @var int dif = 0;
+ *
+ * @param int *sq - array sequencial
+ * @param int *id - array ideal
+ * @param int n - tamanho do array
+ *
+ * @return int dif/2
+ */
+
+int compare(int s, int *sq, int *id)
+{
+    // definir dados
+    int dif = 0;
+    // percorrer array e comparar posição por posição
+    for (int i = 0; i < s; i++)
+    {
+        if (sq[i] != id[i])
+        {
+            dif = dif + 1;
+        }
+    }
+    // return
+    return (dif / 2);
+}
+
+/**
  * Metódo Main
  * Objetivo: Ler linha de comparações, ler inteiro de sequencia,ler sequencia, printar o mínimo de swaps
  * Como: scanf para n cmp, guardar em váriavel, fazer repetição iterativa, scanf de sequencia, comparar com seuquencia ideal, dar o resultado
@@ -23,7 +54,7 @@
  * @var int mov - numero de movimentações
  *
  * @version 1 - ler número de comp e fazer repetição iterativa - OK
- * @version 2 - chamar metodo de comparação entre arrays e retornar resposta - 
+ * @version 2 - chamar metodo de comparação entre arrays e retornar resposta -
  */
 
 int main()
@@ -33,6 +64,7 @@ int main()
     int s = 0;
     int mov = 0;
     int nm = 0;
+
     // ler numero de comparações
     scanf("%i", &comp);
     getchar();
@@ -52,6 +84,9 @@ int main()
             getchar();
             sq[i] = nm;
         }
+        // chamar metódo de comparação entre arrays
+        mov = compare(s, sq, id);
+        printf("%i\n", mov);
     }
 }
 /**
