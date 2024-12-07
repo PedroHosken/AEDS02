dc263d9f670b7348e01835c21c81d14a22487d5b
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2024-12-07 19:13:21 -0300

    inicio da prova 03

---
+++ Provas/REVP03/AB.java
@@ -0,0 +1,33 @@
+package Provas.REVP03;
+
+/**
+ * Questão 01 do semestre passado
+ * 
+ * @author Pedro Hosken Fernandes Guimarães
+ * @version 1.0 - 07/12/2024
+ */
+
+// ---- Classe Nó ---- //
+class No {
+    public int elemento;
+    public No esq, dir;
+}
+
+// ---- Classe da Árvore ---- //
+public class AB {
+    // dados
+    private No raiz;
+
+    // metódos da Questão
+    /**
+     * Metódo isMax
+     * Objetivo: comparar o valor da altura da árvore com o valor obtido pelo metódo
+     * 
+     * @param valor
+     * @return boolean is
+     */
+    public boolean isMax(double valor) {
+        // Implemente o m´etodo aqui
+    }
+
+}
